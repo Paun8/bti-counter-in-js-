33fec71e0299c02d6d7664576acef3e72038ba2e
Paun8 <96291711+Paun8@users.noreply.github.com>
2022-03-21 19:08:11 +0100

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,17 @@
+
+const marksWeight = '78';
+const johnsWeight = '92';
+
+const marksHeight = '1.69';
+const johnsHeight = '1.92';
+
+const marksBMI =  marksWeight / (marksHeight ** 2);
+
+const johnsBMI =  johnsWeight / (johnsHeight ** 2);
+
+console.log(marksBMI , johnsBMI);
+
+const markHigherBMI = marksBMI > johnsBMI
+
+console.log(marksBMI, johnsBMI, markHigherBMI);
+
